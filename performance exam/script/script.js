6d0b3183fe0ef16758a2f1c1e0929d723fc42f51
catdrinkscoffee <99078230+catdrinkscoffee@users.noreply.github.com>
2022-06-20 14:15:07 +0900

    Update script.js

--- performance exam/script/script.js
+++ performance exam/script/script.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
     "use strict";
 
+    $(".section--modal").hide();
+
+    $(".close-btn").click(function () {
+        // 모달창이 꺼지는 기능
+        $(".section--modal").toggle();
+    });
+
+    $(".modal-trigger").click(function () {
+        // 모달창이 켜지는 기능
+        $(".section--modal").toggle();
+    });
+
+    var slideIndex = 0,
+        slideLen = 3;
+
+    window.setInterval(function() {
+        if(slideIndex == slideLen) slideIndex = 0;
+        var slidePosition = slideIndex * (-800) + "px";
+        $(".slide").animate({left : slidePosition});
+        slideIndex = slideIndex + 1;
+    }, 3000);
 });
