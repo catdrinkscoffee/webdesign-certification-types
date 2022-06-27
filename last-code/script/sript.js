af0326df073055222f15935f7acb5d9aa7b2cb74
catdrinkscoffee <99078230+catdrinkscoffee@users.noreply.github.com>
2022-06-27 15:21:49 +0900

    Create sript.js

---
+++ last-code/script/sript.js
@@ -0,0 +1,24 @@
+// var slideIndex = 0;
+
+// window.setInterval(function() {
+//     if(slideIndex == 3) slideIndex = 0;
+//     slideIndex++;
+// }, 2500);
+
+// var slideIndex = 0;
+
+// window.setInterval(function() {
+//     if(slideIndex == 3) slideIndex = 0;
+//     $(".slider a").fadeOut();
+//     $(".slider a").eq(slideIndex).fadeIn();
+//     slideIndex++;
+// }, 2500);
+
+// var slideIndex = 0;
+
+// window.setInterval(function() {
+//     if(slideIndex == 3) slideIndex = 0;
+//     var slidePosition = slideIndex * (-1000) + "px";
+//     $(".slider-paper").animate({left: slidePosition});
+//     slideIndex++;
+// }, 2500);
